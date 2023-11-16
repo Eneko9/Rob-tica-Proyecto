e9bcbce6e10d28122a04ec7fce42704cfb40b049
Eneko9 <eneko.eguiguren@opendeusto.es>
2023-11-16 20:43:38 +0100

    aruco creado

--- bordes2.py
+++ bordes2.py
@@ -1,114 +1,40 @@
-"""import cv2
-import numpy as np
-
-img = cv2.imread(r"C:\Master MUCSI\computerVision\Vision-Robotica\cap.jpeg")
-# Inicializar la captura de video desde la cámara
-
-# Redimensionar el fotograma capturado
-#frame = cv2.pyrDown(frame)
-
-x1, y1 = 248, 130
-x2, y2 = 457, 280
-
-# Recortar la imagen
-imagen_recortada = img[y1:y2, x1:x2]
-
-nuevas_dimensiones = (int(img.shape[1]/2), int(img.shape[0]/2))
-
-# Redimensionar la imagen
-img = cv2.resize(img, nuevas_dimensiones)
-
-ret, thresh = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 5, 255, 1)
-contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-black = np.zeros_like(img)
-
-for cnt in contours:
-    epsilon = 0.02 * cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, epsilon, True)
-    hull = cv2.convexHull(cnt)
-    area = cv2.contourArea(cnt)
-    cv2.drawContours(black, [cnt], -1, (0, 255, 0), 2)
-    # cv2.drawContours(black, [approx], -1, (255, 255, 0), 2)
-    # cv2.drawContours(black, [hull], -1, (0, 0, 255), 2)
-
-cv2.imshow("bordes", black)
-cv2.imshow("img", img)
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-"""
-
-
-""""import cv2
-import numpy as np
-
-img = cv2.imread(r"C:\Master MUCSI\computerVision\Vision-Robotica\cap.jpeg")
-
-x1, y1 = 248, 130
-x2, y2 = 457, 280
-
-imagen_recortada = img[y1:y2, x1:x2]
-
-nuevas_dimensiones = (int(img.shape[1]/2), int(img.shape[0]/2))
-img = cv2.resize(img, nuevas_dimensiones)
-
-ret, thresh = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 5, 255, 1)
-contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-black = np.zeros_like(img)
-
-# ...
-
-# Almacenar las coordenadas de los puntos más pequeños
-min_point_coordinates = []
-
-for cnt in contours:
-    epsilon = 0.02 * cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, epsilon, True)
-    hull = cv2.convexHull(cnt)
-    area = cv2.contourArea(cnt)
-
-    if area < 50:  # Puedes ajustar este umbral según tus necesidades
-        # Obtener las coordenadas del rectángulo delimitador
-        x, y, w, h = cv2.boundingRect(cnt)
-        min_point_coordinates.append((x, y))
-
-        # Dibujar un círculo en el punto más pequeño
-        cv2.circle(black, (x, y), 5, (0, 0, 255), -1)
-
-    cv2.drawContours(black, [cnt], -1, (0, 255, 0), 2)
-
-# ...
-
-# Imprimir las coordenadas de los puntos más pequeños
-for coord in min_point_coordinates:
-    print("Coordenadas del punto más pequeño:", coord)
-
-cv2.imshow("bordes", black)
-cv2.imshow("img", img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
 import cv2
 import numpy as np
 import yaml
 
-img = cv2.imread(r"C:\Master MUCSI\computerVision\Vision-Robotica\cap.jpeg")
+# Lee la imagen
+img = cv2.imread("yes2.png")
 
-x1, y1 = 248, 130
-x2, y2 = 457, 280
-
-imagen_recortada = img[y1:y2, x1:x2]
-
+# Redimensionar la imagen (opcional)
 nuevas_dimensiones = (int(img.shape[1]/2), int(img.shape[0]/2))
 img = cv2.resize(img, nuevas_dimensiones)
 
+# Convierte la imagen a escala de grises
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.medianBlur(gray, 5)
 
+# Carga el diccionario de marcadores
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+
+# Inicializa el detector de marcadores
+parameters = cv2.aruco.DetectorParameters_create()
+
+# Detecta los marcadores en la imagen
+corners_aruco, ids_aruco, _ = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+
+# Almacena las coordenadas de los marcadores ArUco en un array
+coordinates_aruco = []
+
+# Si se detectan marcadores ArUco
+if ids_aruco is not None:
+    # Dibuja los contornos y los ID de los marcadores en la imagen
+    cv2.aruco.drawDetectedMarkers(img, corners_aruco, ids_aruco)
+
+    # Extrae las coordenadas de los marcadores ArUco
+    for marker_id, corner in zip(ids_aruco, corners_aruco):
+        x, y = np.mean(corner[0], axis=0)
+        coordinates_aruco.append((int(x), int(y)))
+
+# Detecta círculos en la imagen
 circles = cv2.HoughCircles(
     gray,
     cv2.HOUGH_GRADIENT,
@@ -120,25 +46,39 @@ circles = cv2.HoughCircles(
     maxRadius=30
 )
 
+# Almacena las coordenadas de los círculos en un array
+coordinates_circles = []
+
+# Si se detectan círculos
 if circles is not None:
     circles = np.uint16(np.around(circles))
     
-    # Almacenar las coordenadas en un array
-    coordinates_array = []
-
+    # Extrae las coordenadas de los círculos en el mismo array
     for i in circles[0, :]:
         x, y = i[0], i[1]
-        coordinates_array.append((x, y))
-
+        coordinates_circles.append((x, y))
         # Dibujar el círculo externo
         cv2.circle(img, (x, y), i[2], (0, 255, 0), 2)
         # Dibujar el centro del círculo
         cv2.circle(img, (x, y), 2, (0, 0, 255), 3)
 
-    # Guardar las coordenadas en un archivo YAML
-    with open('pose.yaml', 'w') as yaml_file:
-        yaml.dump(coordinates_array, yaml_file)
+# Guardar las coordenadas en archivos YAML separados
+with open('pose_aruco.yaml', 'w') as yaml_file_aruco:
+    yaml.dump(coordinates_aruco, yaml_file_aruco)
 
-cv2.imshow("Círculos detectados", img)
+with open('pose_circles.yaml', 'w') as yaml_file_circles:
+    yaml.dump(coordinates_circles, yaml_file_circles)
+
+# Imprimir las coordenadas por separado
+print("Coordenadas de Marcadores ArUco:")
+for coord_aruco in coordinates_aruco:
+    print(coord_aruco)
+
+print("\nCoordenadas de Círculos:")
+for coord_circles in coordinates_circles:
+    print(coord_circles)
+
+# Mostrar la imagen con los marcadores ArUco y círculos detectados
+cv2.imshow("Marcadores ArUco y Circulos detectados", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
